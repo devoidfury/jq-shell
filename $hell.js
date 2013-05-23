b6b004d939643ae18763f1285a104c2f74d32aa7
devoidfury <tom@furycodes.com>
2013-05-23 08:28:31 -0500

    initial

---
+++ $hell.js
@@ -0,0 +1,151 @@
+(function ($) {
+    'use strict';
+    $.fn.$hell = function(opts) {
+        this.length && this.each(function() {
+            $(this).append(Shell(opts).$hell);
+        });
+        return this;
+    };
+
+    function Shell(opts) {
+        if (!(this instanceof Shell)) return new Shell(opts);
+
+        opts = opts || {};
+        this.opts = $.extend({}, this.opts, opts);
+        this.cmds = $.extend({}, opts.commands || {}, this.cmds);
+
+        this.hist = [];
+        this.hist_index = 0;
+
+        this.$hell = $('<div class="shell"></div>');
+
+        var self = this;
+        this.$hell.on('click', function(e) {
+            return self.focus(e);
+        });
+        this.$hell.on('keydown', function(e) {
+            return self.preventDeletePrompt(e)
+        });
+        this.$hell.on('keyup', function(e) {
+            return self.keyHandler(e);
+        });
+
+        this.$in = $('<input value="'+this.opts.prompt+'">');
+        this.$hell.append(this.$in);
+
+        this.writeLn(this.opts.greeting, true);
+    }
+
+    // default options
+    Shell.prototype.opts = {
+        prompt: '$ ',
+        greeting: 'Welcome to the $hell.<br><br>' +
+            'Use the "help" command to list available commands.<br>' +
+            'Press ESC or use exit command to exit.',
+        unknown: "command '{{cmd}}' not found"
+    };
+
+    // default commands
+    Shell.prototype.cmds = {
+        // print list of available commands
+        help: function(shell, args, cb) {
+            var coms = [];
+            for (var command in shell.cmds) { coms.push(command); }
+            cb(0, coms.join('<br>'))
+        },
+
+        // exit $hell
+        exit: function(shell) {
+            shell.$in.val(shell.opts.prompt);
+            shell.$hell.slideUp();
+        }
+    };
+
+    /* write a line of output.
+     *
+     * line: text to write
+     * safe: true if line should be written as html
+     * code: indicates success/failure. success = 0, all other values are failure
+     */
+    Shell.prototype.writeLn = function writeLn(line, safe, code) {
+        var $p = $('<p></p>')[safe ? 'html' : 'text'](line);
+
+        if (typeof code !== 'undefined') $p.addClass(code === 0 ? 'green' : 'red');
+
+        $p.insertBefore(this.$in);
+    };
+
+    // focus the prompt
+    Shell.prototype.focus = function (){
+        var len = this.$in.val().length;
+
+        this.$in.focus();
+        this.$in[0].setSelectionRange ?
+            this.$in[0].setSelectionRange(len, len) :
+            this.$in.val(this.$in.val());
+    };
+
+    // don't allow user to delete prompt
+    Shell.prototype.preventDeletePrompt = function(e) {
+        if (e.keyCode === 8 && this.$in.val().length <= this.opts.prompt.length) {
+            return false;
+        }
+    };
+
+    Shell.prototype.keyHandler = function(e) {
+        var val = this.$in.val(),
+            self = this;
+
+        // make sure prompt is always there
+        if (val.indexOf(this.opts.prompt) !== 0) {
+            this.$in.val(this.opts.prompt);
+            return false;
+        }
+
+        switch (e.keyCode) {
+            // exit on <ESC>
+            case 27:
+                return this.cmds.exit(this);
+
+            // execute command on <enter>
+            case 13:
+                this.execute(val, function(cmd, code, out) {
+                    self.writeLn(cmd, false, code);
+                    out && self.writeLn(out, true);
+                    self.$in.val(self.opts.prompt);
+                });
+                break;
+
+            // up arrow -- backwards in history
+            case 38:
+                if (this.hist_index !== 0) {
+                    this.$in.val(this.opts.prompt + this.hist[--this.hist_index]);
+                    this.focus();
+                }
+                break;
+
+            // down arrow -- forwards in history
+            case 40:
+                if (this.hist_index !== this.hist.length) {
+                    this.$in.val(this.opts.prompt + (this.hist[++this.hist_index] || ''));
+                    this.focus();
+                }
+                break;
+        }
+    };
+
+    // execute a command
+    Shell.prototype.execute = function(cmd, cb) {
+        var args = cmd.substring(this.opts.prompt.length, cmd.length - this.opts.prompt.length + 2).split(' ');
+        args[0] && this.hist.push(args.join(' '));
+        this.hist_index = this.hist.length;
+
+        args[0] ?
+            typeof this.cmds[args[0]] === 'function' ?
+                this.cmds[args[0]](this, args, function(code, out) {
+                    cb(cmd, code, out);
+                }) :
+                cb(cmd, 1, this.opts.unknown.split('{{cmd}}').join(args[0])) :
+            cb(cmd, 0);
+    };
+}(jQuery));
